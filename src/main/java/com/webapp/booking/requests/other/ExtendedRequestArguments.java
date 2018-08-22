49faa966bcc3adf02f5ee46bcda0866e43a08481
Elizaveta Lavelina <lavelinae@gmail.com>
2018-08-22 12:56:30 +0300

    TASK-1-2-BE: added template for book button

---
+++ src/main/java/com/webapp/booking/requests/other/ExtendedRequestArguments.java
@@ -0,0 +1,26 @@
+package com.webapp.booking.requests.other;
+
+import com.webapp.booking.enums.RoomType;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class ExtendedRequestArguments {
+    private Integer guestAmount;
+    private RoomType roomType;
+    private Double price;
+    private Double discount;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date checkInDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date checkOutDate;
+    private String roomDescription;
+    private String hotelName;
+    private String hotelAddress;
+    private String hotelDescription;
+    private Integer hotelRating;
+}
