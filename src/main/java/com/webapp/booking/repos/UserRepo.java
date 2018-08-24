c349890f7f32f721ab581ee3bd90a2781cc47833
Victor Kokachev <victor_kokachev@epam.com>
2018-08-24 18:09:54 +0300

    TASK-15: added search methods

--- src/main/java/com/webapp/booking/repos/UserRepo.java
+++ src/main/java/com/webapp/booking/repos/UserRepo.java
@@ -1,4 +1,29 @@
 package com.webapp.booking.repos;
 
+import com.webapp.booking.entities.UserEntity;
+import com.webapp.booking.repos.util.UserEntityRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class UserRepo {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    UserEntityRowMapper userEntityRowMapper;
+
+    public List<UserEntity> getAllUsers() {
+        String sql = "SELECT user_id, login, password, name, role FROM user";
+        return jdbcTemplate.query(sql, userEntityRowMapper);
+    }
+
+    public List<UserEntity> getUserByID(Integer userID) {
+        String sql = "SELECT user_id, login, password, name, role FROM user WHERE user_id = " + userID;
+        return jdbcTemplate.query(sql, userEntityRowMapper);
+    }
 }
--- src/main/java/com/webapp/booking/services/UserService.java
+++ src/main/java/com/webapp/booking/services/UserService.java
@@ -1,6 +1,7 @@
 package com.webapp.booking.services;
 
 import com.webapp.booking.entities.UserEntity;
+import com.webapp.booking.repos.UserRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,22 @@ import java.util.List;
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class UserService {
 
+    @Autowired
+    UserRepo userRepo;
+
     public List<UserEntity> getAllUsers() {
-        return null;
+        return userRepo.getAllUsers();
     }
 
-    public UserEntity getUserByID(int userID) {
-        return null;
+    public UserEntity getUserByID(Integer userID) {
+        List<UserEntity> userByID = userRepo.getUserByID(userID);
+        if (userByID.size() == 0) {
+            throw new RuntimeException("There is no user with this ID");
+        } else if (userByID.size() > 1) {
+            throw new RuntimeException("There are more than one user with this ID");
+        } else {
+            return userByID.get(0);
+        }
     }
 
     public void updateUser() {
